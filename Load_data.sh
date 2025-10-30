5f20f4c574d9ed3ed0191938bcd2cab97f3b3ce7
Naga Bala Subrahmanyam Gollapalli <balus98076@gmail.com>
2025-10-30 17:15:10 +0530

    Delete data loading script commands
    Removed database migration and data loading commands.

--- Load_data.sh
+++ Load_data.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata User.json UserProfile.json UserAddress.json CustomUserManager.json \
-Brand.json Category.json ProductManager.json ProductQuerySet.json ProductVariation.json Product.json \
-Order.json OrderItem.json Review.json Wishlist.json \
-VendorUser.json VendorUserManager.json Vendor.json VendorBrand.json VendorCategory.json VendorProduct.json
+
